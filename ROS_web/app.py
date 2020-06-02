3277a44eaef9464d207f9d7bc52cedccfc88010b
1802669805@qq.com <1802669805@qq.com>
2020-06-02 20:02:11 +0800

    添加建图 删除跟随移动

--- ROS_web/app.py
+++ ROS_web/app.py
@@ -12,18 +12,18 @@ app.config['SECRET_KEY'] = '123456'  # flask生成密钥
 @app.route('/')
 def goto_index():
     # sendMessage.tcp_link_conn()
-    return render_template('login.html')  # 直接跳转到首页
+    return render_template('login.html')  # 直接跳转到登陆页面
 
 
 @app.route('/login')
 def get_login_request():
-    username = request.args.get('loginUsername')
-    password = request.args.get('loginPassword')
+    username = request.args.get('loginUsername')  # 从页面中获取用户名
+    password = request.args.get('loginPassword')  # 从页面中获取密码
     if username:
-        result = dbmessage.User.query(username)
+        result = dbmessage.User.query(username)  # 查询数据库是否有本用户
         if result and result.user_pwd == password:  # 用户名和密码与数据库中数据相对应
-            session['name'] = username
-            return redirect(url_for('index', name=username, _external=True))
+            session['name'] = username  # 将用户id存入缓存
+            return redirect(url_for('index', name=username, _external=True))  # 跳转到主页
         else:
             flash('用户名或密码不正确！')
             return render_template('login.html')
@@ -35,21 +35,20 @@ def add_user():
     username = request.args.get('registerUsername')
     password = request.args.get('registerPassword')
     if username:
-        result = dbmessage.User.query(username)
+        result = dbmessage.User.query(username)  # 查询数据库是否有本用户
         if result:
             flash('邮箱已被注册！')
             return render_template('register.html')
         else:
             user = dbmessage.User(username, password)
-            user.insert()
+            user.insert()  # 数据库中无此用户，将此用户加入数据库中
             flash('注册成功！请登录')
-            return render_template('login.html')
+            return render_template('login.html')  # 跳转到登录页面
     return render_template('register.html')
 
 
 @app.route('/index')
 def index():
-    user_id = session.get('name')
     pattern = request.args.get('pattern')
     action = request.args.get('action')
     if action:
